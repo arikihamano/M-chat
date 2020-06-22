0caaa448a53136e68d5a89dc5a0aa7b854731709
arikihamano <arikid_boxing_0408@yahoo.co.jp>
2020-06-23 08:45:09 +0900

    added edit-page for user-profile cf.4-1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i(name self_introduction img_name))
+    devise_parameter_sanitizer.permit(:account_update, keys: %i(name self_introduction img_name))
   end
 
 end
