c0efe7c60dece71ef6323e0ee383a974f1dc3494
arikihamano <arikid_boxing_0408@yahoo.co.jp>
2020-06-22 09:00:20 +0900

    added columns(name self_introduction img_name to User model cf.2-6)

---
+++ db/migrate/20200621235553_add_column_to_user.rb
@@ -0,0 +1,7 @@
+class AddColumnToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :name, :string, null: false, default: '', limit: 100
+    add_column :users, :self_introduction, :string, limit: 500
+    add_column :users, :img_name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_223202) do
+ActiveRecord::Schema.define(version: 2020_06_21_235553) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2020_06_21_223202) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name", limit: 100, default: "", null: false
+    t.string "self_introduction", limit: 500
+    t.string "img_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
