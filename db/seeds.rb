c67e6ec9354e01733190e6b7ffce2a08482ec9a8
arikihamano <arikid_boxing_0408@yahoo.co.jp>
2020-06-23 09:03:04 +0900

    edited seeds.rb for dummy user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p "create Users"
+User.create!(
+  email: 'user1@example.com',
+  password: 'aaaaaaaa',
+  name: 'オリバー',
+  self_introduction: 'オリバーです',
+  img_name: open("#{Rails.root}/db/dummy_img/1.jpg")
+)
+User.create!(
+  email: 'user2@example.com',
+  password: 'aaaaaaaa',
+  name: 'ハリー',
+  self_introduction: 'ハリーです',
+  img_name: open("#{Rails.root}/db/dummy_img/2.jpg")
+)
+User.create!(
+  email: 'user3@example.com',
+  password: 'aaaaaaaa',
+  name: 'オリヴィア',
+  self_introduction: 'オリヴィアです',
+  img_name: open("#{Rails.root}/db/dummy_img/3.jpg")
+)
+User.create!(
+  email: 'user4@example.com',
+  password: 'aaaaaaaa',
+  name: 'リリー',
+  self_introduction: 'リリーです',
+  img_name: open("#{Rails.root}/db/dummy_img/4.jpg")
+)
