196e13d01d2933f1eb131ad705e4fe485ae4af31
arikihamano <arikid_boxing_0408@yahoo.co.jp>
2020-06-23 20:39:25 +0900

    add users/index.html.erb cf.5-2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: %i(name self_introduction img_name))
   end
 
+  def after_sign_in_path_for(resource)
+    users_path
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def index
+    @users= User.where.not(id:current_user.id)
+  end
+
   def show
     @user = User.find_by(id: params[:id])
   end
