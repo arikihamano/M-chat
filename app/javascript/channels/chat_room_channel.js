95ca312e30d93c93d09bf8ec91cafb8bbc8847c4
arikihamano <arikid_boxing_0408@yahoo.co.jp>
2020-06-29 21:44:16 +0900

    text the connection with console log

--- app/javascript/channels/chat_room_channel.js
+++ app/javascript/channels/chat_room_channel.js
@@ -13,7 +13,7 @@ consumer.subscriptions.create(
     },
 
     received(data) {
-      // Called when there's incoming data on the websocket for this channel
+      console.log(data);
     },
 
     speak: function () {
