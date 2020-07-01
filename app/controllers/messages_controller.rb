598cece88837d740df75819981ba0796b1639fb4
arikihamano <arikid_boxing_0408@yahoo.co.jp>
2020-07-01 23:24:29 +0900

    tried to replace form_with instead of input in message/show.html.erb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,5 +5,6 @@ class MessagesController < ApplicationController
     @chat_room_user = chat_room.chat_room_users.
       where.not(user_id: current_user.id).first.user
     @chat_messages = ChatMessage.where(chat_room: chat_room).order(:created_at)
+    @chat_message = ChatMessage.new
   end
 end
