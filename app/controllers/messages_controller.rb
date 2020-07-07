a5eaa331a28b78aa8b9ead9bc5f6dde04fbc4603
mozy okubo <mozy.okubo@gmail.com>
2020-07-07 13:36:51 +0900

    fix

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,23 +1,20 @@
+# frozen_string_literal: true
+
 class MessagesController < ApplicationController
-
-  require "pry"
-
   def show
     # チャット相手の情報を取得する
-    chat_room = ChatRoom.find_by(id: params[:id])
-    @chat_room_user = chat_room.chat_room_users.
-      where.not(user_id: current_user.id).first.user
-    @chat_messages = ChatMessage.where(chat_room: chat_room).order(:created_at)
-    
-    binding.pry
-    
+    @chat_room = ChatRoom.find_by(id: params[:id])
+    @chat_room_user = @chat_room.chat_room_users
+                                .where.not(user_id: current_user.id).first.user
+    @chat_messages = ChatMessage.where(chat_room_id: @chat_room).order(:created_at)
+    @chat_message = 'hi'
   end
 
   def create
     @chat_message = ChatMessage.new
     @chat_message.user_id = current_user.id # ここがフォームからきた値をモデルに移し替える
-    @chat_message.chat_room_id = chat_room.id # ここは間違っているかもだが、そもそもフォームが表示できないので一度スキップ
+    @chat_message.chat_room_id = params[:id] # ここは間違っているかもだが、そもそもフォームが表示できないので一度スキップ
     @chat_message.message = params[:content]
-    @chat_message.save()
+    @chat_message.save
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
-  root "top#index"
-  resources :users, only: %i(index show)
+  root 'top#index'
+  resources :users, only: %i[index show]
 
-  resources :chat, only: %i(create show)
+  resources :chat, only: %i[create show]
 
   resources :messages
+  post '/messages/:id', to: 'messages#create'
 end
