0553e599148d81edc2337ca3e24d65354773f101
arikihamano <arikid_boxing_0408@yahoo.co.jp>
2020-07-10 22:58:02 +0900

    video1 1:18:00 completed very basic chat app

--- app/javascript/channels/chat_room_channel.js
+++ app/javascript/channels/chat_room_channel.js
@@ -1,25 +1,42 @@
 import consumer from "./consumer";
 
-consumer.subscriptions.create(
-  { channel: "ChatRoomChannel", chat_room_id: 1 },
-  {
-    connected() {
-      console.log("connected!!!");
-      // Called when the subscription is ready for use on the server
-    },
+document.addEventListener("turbolinks:load", () => {
+  const chat_room_element = document.getElementById("chat_room-id");
+  const chat_room_id = Number(
+    chat_room_element.getAttribute("data-chat_room-id")
+  );
+  consumer.subscriptions.create(
+    { channel: "ChatRoomChannel", chat_room_id: chat_room_id },
+    {
+      connected() {
+        console.log("connected to" + chat_room_id);
+        // Called when the subscription is ready for use on the server
+      },
 
-    disconnected() {
-      // Called when the subscription has been terminated by the server
-    },
+      disconnected() {
+        // Called when the subscription has been terminated by the server
+      },
 
-    received(data) {
-      console.log(data);
-      const messageContainer = document.getElementById("chat-messages");
-      messageContainer.innerHTML = messageContainer.innerHTML + data.html;
-    },
+      received(data) {
+        console.log(data);
+        const user_element = document.getElementById("user-id");
+        const user_id = Number(user_element.getAttribute("data-user-id"));
+        // 以下はメッセージの送信者によって、クラスを付与してメッセージの配置を変更する場合
+        // let html;
 
-    speak: function () {
-      return this.perform("speak");
-    },
-  }
-);
+        // if (user_id === data.message.user_id) {
+        //   html = data.mine
+        // } else {
+        //   html = data.theirs
+        // }
+
+        const messageContainer = document.getElementById("chat-messages");
+        messageContainer.innerHTML = messageContainer.innerHTML + data.html;
+      },
+
+      speak: function () {
+        return this.perform("speak");
+      },
+    }
+  );
+});
