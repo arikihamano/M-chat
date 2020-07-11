d92dcf4a538aa4e55af7f238afa7ac26832fc685
arikihamano <arikid_boxing_0408@yahoo.co.jp>
2020-07-11 12:26:33 +0900

    added basic user-search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
 
   def index
-    @users= User.where.not(id:current_user.id)
+    
+    @users = if params[:search]
+      @users= User.where.not(id:current_user.id).where('name LIKE ?', "%#{params[:search]}%")
+    else
+      @users= User.where.not(id:current_user.id)
+    end
   end
 
   def show
