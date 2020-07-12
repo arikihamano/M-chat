1e6a4746ab5c4db63a19cefe3cce01c25f628749
arikihamano <arikid_boxing_0408@yahoo.co.jp>
2020-07-12 12:21:44 +0900

    implemented chat room validation with find_by method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,11 +2,15 @@
 
 class MessagesController < ApplicationController
   def show
-    # チャット相手の情報を取得する
+      # チャット相手の情報を取得する
     @chat_room = ChatRoom.find_by(id: params[:id])
-    @chat_room_user = @chat_room.chat_room_users
-                                .where.not(user_id: current_user.id).first.user
-    @chat_messages = ChatMessage.where(chat_room_id: @chat_room).order(:created_at)
+    if @chat_room.chat_room_users.find_by(user_id: current_user.id)
+      @chat_room_user = @chat_room.chat_room_users
+                                  .where.not(user_id: current_user.id).first.user
+      @chat_messages = ChatMessage.where(chat_room_id: @chat_room).order(:created_at)
+    else
+      redirect_to users_path
+    end
   end
 
   def create
@@ -25,5 +29,10 @@ class MessagesController < ApplicationController
     # ActionCable.server.broadcast "room_channel_#{@chat_message.chat_room_id}", html: html
 
     SendMessageJob.perform_later(@chat_message)
+  end
+
+  def correct_user?
+    # カレントユーザーのIDを取得
+    # カレントユーザーのIDがチャットルームに入っているかどうかを返す
   end
 end
