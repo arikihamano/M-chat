35045c78ffa65561315f0f6037e266e2e7bba047
arikihamano <arikid_boxing_0408@yahoo.co.jp>
2020-07-21 21:43:01 +0900

    mozy will check the code

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :user do
-    
+  end
+
+  factory :user_tanaka, class: 'User' do
+    name { '田中 太郎' }
+    email { 'taro.tanaka@example.com' }
+    self_introduction { 'こんにちは' }
+    encrypted_password { 'password' }
+    img_name { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/example.jpg')) }
+  end
+
+  factory :user_yamada, class: 'User' do
+    name { '山田 花子' }
+    email { 'hanako.yamada@example.com' }
+    self_introduction { 'こんにちは' }
+    encrypted_password { 'password' }
+    img_name { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/example.jpg')) }
   end
 end
