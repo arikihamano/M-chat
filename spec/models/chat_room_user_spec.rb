c9b9e562d550c59519d97cab0e82e4d43e9b78ee
arikihamano <arikid_boxing_0408@yahoo.co.jp>
2020-07-23 11:42:41 +0900

    started to write the test for chat_room_user

--- spec/models/chat_room_user_spec.rb
+++ spec/models/chat_room_user_spec.rb
@@ -1,5 +1,91 @@
 require 'rails_helper'
 
 RSpec.describe ChatRoomUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "正常系の機能" do
+    context 'ルームを作成する' do
+      it '正しくルームとの紐付けを作成できること' do
+        user_tanaka = User.new(
+          name: '田中　太郎',
+          email: 'taro.tanaka@example.com',
+          self_introduction: 'こんにちは',
+          password: 'password',
+          img_name: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/example.jpg'))
+        )
+
+        user_tanaka.save
+
+        chat_room_1 = ChatRoom.new()
+        chat_room_1.save
+
+        chat_room_user = ChatRoomUser.new(
+          chat_room_id: 1,
+          user_id: 1
+        )
+        expect(chat_room_user).to be_valid 
+
+        chat_room_user.save
+      end
+    end
+  end
+
+  describe "入力項目の有無" do
+    context '必須項目であること' do
+      it 'チャットルームIDが必須であること' do
+        user_tanaka = User.new(
+          name: '田中　太郎',
+          email: 'taro.tanaka@example.com',
+          self_introduction: 'こんにちは',
+          password: 'password',
+          img_name: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/example.jpg'))
+        )
+
+        user_tanaka.save
+
+        chat_room_1 = ChatRoom.new()
+        chat_room_1.save
+
+        new_chat_room_user = ChatRoomUser.new(chat_room_id: 1)
+        expect(new_chat_room_user).not_to be_valid  
+        
+      end
+      it 'ユーザーIDが必須であること' do
+        user_tanaka = User.new(
+          name: '田中　太郎',
+          email: 'taro.tanaka@example.com',
+          self_introduction: 'こんにちは',
+          password: 'password',
+          img_name: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/example.jpg'))
+        )
+
+        user_tanaka.save
+
+        chat_room_1 = ChatRoom.new()
+        chat_room_1.save
+
+        new_chat_room_user = ChatRoomUser.new(user_id: 1)
+        expect(new_chat_room_user).not_to be_valid  
+      end
+      it '登録できないこと' do
+        user_tanaka = User.new(
+          name: '田中　太郎',
+          email: 'taro.tanaka@example.com',
+          self_introduction: 'こんにちは',
+          password: 'password',
+          img_name: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/example.jpg'))
+        )
+
+        user_tanaka.save
+
+        chat_room_1 = ChatRoom.new()
+        chat_room_1.save
+
+        new_chat_room_user = ChatRoomUser.new
+
+        expect(new_chat_room_user.save).to be_falsey
+      end
+    end
+    
+  end
+  
+
 end
