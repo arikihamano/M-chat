6b2f5fb70a7325c471a4a064d3648508067273f6
arikihamano <arikid_boxing_0408@yahoo.co.jp>
2020-07-23 19:37:38 +0900

    added the test code for required column in ChatMessage

--- spec/models/chat_message_spec.rb
+++ spec/models/chat_message_spec.rb
@@ -25,6 +25,48 @@ RSpec.describe ChatMessage, type: :model do
         )
         expect(message).to be_valid
         message.save
+
+        created_message =  ChatMessage.find(1)
+        expect(created_message.chat_room_id).to eq(1)
+        expect(created_message.user_id).to eq(1)  
+      end
+    end
+  end
+
+  describe "必須項目の有無" do
+    context "それぞれの項目が必須であること" do
+      it "ユーザーIDが必須であること" do
+        new_message = ChatMessage.new(
+          chat_room_id: 1,
+          message: "Hello!"
+        )
+        expect(new_message).not_to be_valid
+      end
+      it "チャットルームIDが必須であること" do
+        new_message = ChatMessage.new(
+          user_id: 1,
+          message: "Hello!"
+        )
+        expect(new_message).not_to be_valid
+      end
+      it "メッセージ内容が必須であること" do
+        new_message = ChatMessage.new(
+          chat_room_id: 1,
+          user_id: 1,
+        )
+        expect(new_message).not_to be_valid
+      end
+      it "空文字のメッセージではないこと" do
+        new_message = ChatMessage.new(
+          chat_room_id: 1,
+          user_id: 1,
+          message: ""
+        )
+        expect(new_message).not_to be_valid
+      end
+      it "要件を満たさないメッセージは作成できないこと" do
+        new_message = ChatMessage.new
+        expect(new_message.save).to be_falsey
       end
     end
   end
