82f74d7db4b9f3e6af3b0870c814adb237fb1e8b
arikihamano <arikid_boxing_0408@yahoo.co.jp>
2020-07-23 14:12:34 +0900

    wrote the uniqueness test for chat_roon_user

--- app/models/chat_room_user.rb
+++ app/models/chat_room_user.rb
@@ -1,4 +1,5 @@
 class ChatRoomUser < ApplicationRecord
   belongs_to :chat_room
   belongs_to :user
+  validates :user_id, :uniqueness => {:scope => :chat_room_id}
 end
--- spec/models/chat_room_user_spec.rb
+++ spec/models/chat_room_user_spec.rb
@@ -1,21 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe ChatRoomUser, type: :model do
+  before do
+    user_tanaka = User.new(
+      name: '田中　太郎',
+      email: 'taro.tanaka@example.com',
+      self_introduction: 'こんにちは',
+      password: 'password',
+      img_name: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/example.jpg'))
+    )
+
+    user_tanaka.save
+
+    chat_room_1 = ChatRoom.new()
+    chat_room_1.save
+  end
+
   describe "正常系の機能" do
     context 'ルームを作成する' do
       it '正しくルームとの紐付けを作成できること' do
-        user_tanaka = User.new(
-          name: '田中　太郎',
-          email: 'taro.tanaka@example.com',
-          self_introduction: 'こんにちは',
-          password: 'password',
-          img_name: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/example.jpg'))
-        )
+        # user_tanaka = User.new(
+        #   name: '田中　太郎',
+        #   email: 'taro.tanaka@example.com',
+        #   self_introduction: 'こんにちは',
+        #   password: 'password',
+        #   img_name: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/example.jpg'))
+        # )
 
-        user_tanaka.save
+        # user_tanaka.save
 
-        chat_room_1 = ChatRoom.new()
-        chat_room_1.save
+        # chat_room_1 = ChatRoom.new()
+        # chat_room_1.save
 
         chat_room_user = ChatRoomUser.new(
           chat_room_id: 1,
@@ -31,60 +46,81 @@ RSpec.describe ChatRoomUser, type: :model do
   describe "入力項目の有無" do
     context '必須項目であること' do
       it 'チャットルームIDが必須であること' do
-        user_tanaka = User.new(
-          name: '田中　太郎',
-          email: 'taro.tanaka@example.com',
-          self_introduction: 'こんにちは',
-          password: 'password',
-          img_name: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/example.jpg'))
-        )
+        # user_tanaka = User.new(
+        #   name: '田中　太郎',
+        #   email: 'taro.tanaka@example.com',
+        #   self_introduction: 'こんにちは',
+        #   password: 'password',
+        #   img_name: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/example.jpg'))
+        # )
 
-        user_tanaka.save
+        # user_tanaka.save
 
-        chat_room_1 = ChatRoom.new()
-        chat_room_1.save
+        # chat_room_1 = ChatRoom.new()
+        # chat_room_1.save
 
         new_chat_room_user = ChatRoomUser.new(chat_room_id: 1)
         expect(new_chat_room_user).not_to be_valid  
         
       end
       it 'ユーザーIDが必須であること' do
-        user_tanaka = User.new(
-          name: '田中　太郎',
-          email: 'taro.tanaka@example.com',
-          self_introduction: 'こんにちは',
-          password: 'password',
-          img_name: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/example.jpg'))
-        )
+        # user_tanaka = User.new(
+        #   name: '田中　太郎',
+        #   email: 'taro.tanaka@example.com',
+        #   self_introduction: 'こんにちは',
+        #   password: 'password',
+        #   img_name: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/example.jpg'))
+        # )
 
-        user_tanaka.save
+        # user_tanaka.save
 
-        chat_room_1 = ChatRoom.new()
-        chat_room_1.save
+        # chat_room_1 = ChatRoom.new()
+        # chat_room_1.save
 
         new_chat_room_user = ChatRoomUser.new(user_id: 1)
-        expect(new_chat_room_user).not_to be_valid  
+        expect(new_chat_room_user).not_to be_valid
       end
       it '登録できないこと' do
-        user_tanaka = User.new(
-          name: '田中　太郎',
-          email: 'taro.tanaka@example.com',
-          self_introduction: 'こんにちは',
-          password: 'password',
-          img_name: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/example.jpg'))
-        )
+        # user_tanaka = User.new(
+        #   name: '田中　太郎',
+        #   email: 'taro.tanaka@example.com',
+        #   self_introduction: 'こんにちは',
+        #   password: 'password',
+        #   img_name: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/example.jpg'))
+        # )
 
-        user_tanaka.save
+        # user_tanaka.save
 
-        chat_room_1 = ChatRoom.new()
-        chat_room_1.save
+        # chat_room_1 = ChatRoom.new()
+        # chat_room_1.save
 
         new_chat_room_user = ChatRoomUser.new
 
         expect(new_chat_room_user.save).to be_falsey
       end
     end
-    
+
+    describe "データ作成時の条件" do
+      context "ユーザーとチャットルームの組み合わせの一意性を確認すること" do
+        it '同じユーザーとチャットルームの組み合わせの重複データは作成できないこと' do
+          chat_room_user = ChatRoomUser.new(
+            chat_room_id: 1,
+            user_id: 1
+          )
+          expect(chat_room_user).to be_valid 
+  
+          chat_room_user.save
+
+          chat_room_user_again = ChatRoomUser.new(
+            chat_room_id: 1,
+            user_id: 1
+          )
+          expect(chat_room_user_again).to_not be_valid
+          expect(chat_room_user_again.save).to be_falsey
+        end
+      end
+    end
+
   end
   
 
