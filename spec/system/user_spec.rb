a4aa4608dcf1781781930d34c17fdb759e2af869
arikihamano <arikid_boxing_0408@yahoo.co.jp>
2020-07-30 10:08:06 +0900

    added integration test for user save failed

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -23,4 +23,24 @@ RSpec.describe "Users", type: :system do
       end
     end
   end
+
+  describe '異常' do
+    context "必須項目が未入力" do
+      it "エラーメッセージが表示され、回答できないこと" do
+        visit "/users/sign_up"
+        sleep 2
+        click_button 'はじめる'
+
+        # [Point.6-2-8]回答が完了したか検証します。
+        expect(page).not_to have_content 'ユーザー一覧'
+        expect(page).to have_content 'アカウントを作成'
+        expect(page).to have_content 'メールアドレスを入力してください'
+        expect(page).to have_content 'パスワードを入力してください'
+        expect(page).to have_content 'プロフィール画像を入力してください'
+        expect(page).to have_content 'ユーザー名を入力してください'
+        expect(page).to have_content '確認用パスワードを入力してください'
+        sleep 5
+      end
+    end
+  end
 end
