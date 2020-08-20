36c40a2ec9ae8c975a0b6ecd454e763751fc365e
arikihamano <arikid_boxing_0408@yahoo.co.jp>
2020-08-20 17:16:14 +0900

    bug occured

--- app/javascript/channels/chat_room_channel.js
+++ app/javascript/channels/chat_room_channel.js
@@ -16,23 +16,36 @@ document.addEventListener("turbolinks:load", () => {
 
   // フォームに入力した際の動作
   messageContent.addEventListener("input", () => {
+    // ボタンを有効化する
     button_activation();
   });
+  const documentElement = document.documentElement;
+
+  window.scrollToBottom = () => {
+    window.scroll(0, documentElement.scrollHeight);
+    messageContainer.scroll(0, messageContainer.scrollHeight + 100);
+  };
 
   // 送信ボタンが押された時にボタンを無効化
   messageButton.addEventListener("click", () => {
     messageButton.classList.add("disabled");
+    window.messageContainer = document.getElementById("chat-messages");
+    scrollToBottom();
   });
 
+  // サブスクリプションのためのデータ取得
   const chat_room_element = document.getElementById("chat_room-id");
   const chat_room_id = Number(
     chat_room_element.getAttribute("data-chat_room-id")
   );
+
   consumer.subscriptions.create(
     { channel: "ChatRoomChannel", chat_room_id: chat_room_id },
     {
       connected() {
         console.log("connected to" + chat_room_id);
+        console.log(documentElement.scrollHeight);
+        console.log(messageContainer.scrollHeight);
       },
 
       disconnected() {
